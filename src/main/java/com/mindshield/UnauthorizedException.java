860a5cd562feadd2f3e8473ddcbfe074cd7c3630
UmutKaanBori <kaan94090@gmail.com>
2026-04-26 04:03:19 +0300

    Delete src/main/java/com/mindshield/UnauthorizedException.java

--- src/main/java/com/mindshield/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.mindshield.exceptions;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
